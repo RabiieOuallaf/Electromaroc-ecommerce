6804908d5559941abc5a4e3f5d9fdf8dba567ab0
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2022-12-16 16:24:50 +0100

    Completed hero section

--- view/assets/javascript/script.js
+++ view/assets/javascript/script.js
@@ -1,3 +1,5 @@
+/* == Clearing the search input on hover == */ 
+
 const SearchInput = document.getElementById("searchInput");
 
 SearchInput.addEventListener("focus", _ => {
@@ -11,3 +13,23 @@ SearchInput.addEventListener("blur", _ => {
     SearchInput.setAttribute("placeholder" , "Search product");
 
 });
+
+/* == Animations on scroll ==*/
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach( (entry) => {
+
+        if(entry.isIntersecting){
+            console.log(entry);
+            entry.target.classList.add('show');
+        }else {
+            entry.target.classList.remove('show');
+        }
+    })
+
+})
+
+const heroElements = document.querySelectorAll(".hidden");
+
+heroElements.forEach((element) => observer.observe(element));
