d80e970b657715e2df8ee5fd2783b795e051e691
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2022-12-26 12:30:51 +0100

    Automating documents names 🔥

--- view/index.php
+++ view/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-commerce</title>
+    <title><?php echo SITENAME ?></title>
     <link rel="stylesheet" href="./assets/css/styles.css"></link>
     <link rel="stylesheet" href="./assets/css/tailwind.css">
     <script src="https://cdn.tailwindcss.com"></script>
