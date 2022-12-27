6ba72c4fb1a9cbec6d8545a36daf93fb44aaf4e8
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2022-12-27 11:57:51 +0100

    Update the databse 💾

--- electromaroc.sql
+++ electromaroc.sql
@@ -1,32 +1,75 @@
+DROP DATABASE electromaroc;
+
+
+CREATE DATABASE electromaroc;
+
+USE electromaroc;
+
+create table users(
+	user_id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(55) NOT NULL ,
+    password VARCHAR(55) NOT NULL, 
+    email VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL DEFAULT 'client',
+    primary key(user_id),
+    UNIQUE KEY username(username),
+    UNIQUE KEY email(email),
+    CHECK(role in ('admin', 'client', 'guest'))
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO users(username, password, email, role) VALUES ("Rabie", "Rabieouallaf123", "Rabie@gmail.com", "admin");
+
+SELECT * FROM users;
+
+CREATE TABLE client(
+	client_id INT NOT NULL AUTO_INCREMENT,
+    client_nomcomplet VARCHAR(255) NOT NULL,
+    client_telephone INT NOT NULL,
+    client_email VARCHAR(255) NOT NULL,
+    client_adresse VARCHAR(255) NOT NULL,
+    client_ville VARCHAR(100) NOT NULL,
+    client_codepostal INT NOT NULL,
+    client_pays VARCHAR(100) NOT NULL,
+    PRIMARY KEY (client_id),
+    UNIQUE KEY (client_email),
+    FOREIGN KEY(client_email) references users(email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE admins(
-	Username varchar(25) not null,
-    Password varchar(100) not null
-)
-
-ALTER table admins add id int not null primary key auto_increment;
-
-CREATE TABLE clients(
-
-	Id int primary key not null auto_increment ,
-	Fullname varchar(150) not null , 
-    PhoneNumber int(14) not null,
-    FullAdresse varchar(255) not null,
-    email varchar(150) not null,
-    password varchar(100) not null
+	admin_id INT NOT NULL AUTO_INCREMENT,
+    admin_username VARCHAR(55) NOT NULL,
+    admin_email VARCHAR(255) NOT NULL,
+    admin_password VARCHAR(155) NOT NULL,
+    role VARCHAR(55) NOT NULL DEFAULT ("admin"),
+    primary key(admin_id),
+    UNIQUE KEY (admin_email),
+    UNIQUE KEY (admin_username)
     
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-)
+CREATE TABLE produits(
 
-create table products(
-	
-    ProductId int not null primary key auto_increment ,
-    CodeBar int not null ,
-    ProductName varchar(155) not null,
-    ProductCategorie varchar(55) not null ,
-    ProductDescription varchar(255) not null ,
-    ProductPrice int not null ,
-    client_id int not null,
-    foreign key (client_id) references clients(Id) 
-    
+	produit_id INT NOT NULL AUTO_INCREMENT,
+    produit_name VARCHAR(255) NOT NULL,
+    produit_codebar INT NOT NULL ,
+    produit_refernce VARCHAR(255) NOT NULL,
+    prix_achat float(11) NOT NULL,
+    prix_final float(11) NOT NULL,
+    prix_offer float(11) NOT NULL,
+    produit_quantite int(11) NOT NULL,
+    produit_description varchar(255) NOT NULL,
+    produit_image varchar(255) NOT NULL,
+    PRIMARY KEY (produit_id)
 
-);
+);	
+
+CREATE TABLE cart ( 
+    cart_id int(11) NOT NULL AUTO_INCREMENT,
+    produit_id int(11) NOT NULL,
+    bought int(11) NOT NULL default 0,
+    client_id int(11) NOT NULL,
+    produit_quantity int(11) NOT NULL,
+    Foreign Key (produit_id) REFERENCES produits(produit_id),
+    Foreign Key (client_id) REFERENCES Client(client_id),
+    PRIMARY KEY (cart_id )
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
