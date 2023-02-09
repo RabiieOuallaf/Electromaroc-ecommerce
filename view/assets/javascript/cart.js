be7cd39cc7ead6f2e33bf3fe31a4e98cf1d2c496
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-02-09 18:06:33 +0100

    add some comments to the source code

--- view/assets/javascript/cart.js
+++ view/assets/javascript/cart.js
@@ -3,20 +3,20 @@ const minus_button = document.querySelectorAll("#minusButton");
 
 
 
-
+// loop over plus and minus buttons and add event listener to them 
 
 for(let plus of plus_button){
     plus.addEventListener("click" , _ => {
         
-        plus.nextElementSibling.dataset.quantity++;
-        plus.nextElementSibling.innerHTML = plus.nextElementSibling.dataset.quantity;
+        plus.nextElementSibling.dataset.quantity++; // icrement the value of dataset-quantity property
+        plus.nextElementSibling.innerHTML = plus.nextElementSibling.dataset.quantity; // display the new value to the end user
        
     })
 }
 
 for(let minus of minus_button){
     minus.addEventListener("click", _ => {
-        minus.previousElementSibling.dataset.quantity > 0 && minus.previousElementSibling.dataset.quantity--;
-        minus.previousElementSibling.innerHTML = minus.previousElementSibling.dataset.quantity;
+        minus.previousElementSibling.dataset.quantity > 0 && minus.previousElementSibling.dataset.quantity--; // decrement the value of dataset-quantity property
+        minus.previousElementSibling.innerHTML = minus.previousElementSibling.dataset.quantity; // display the new value to the end user 
     })
 }
